d2f901007853d38dc7a683395bee4301b10fe1a4
Phellipex <phellipex1792@yahoo.es>
2016-04-19 12:08:19 -0500

    Updated the controller file (logic.js) to make it more "JavaScriptic" by using anonymous functions as object properties instead of just having them as named functions hanging around. Updated the HTML file to call the appController (the object containing said functions).

--- js/logic.js
+++ js/logic.js
@@ -1,122 +1,123 @@
-var emptyFieldMessage = 'no se puede hacer el cálculo porque faltan datos, asegúrese de llenar todos los campos con letras o números según sea el caso.';
+appController = {
 
-function clearPlaceholder(placeholderId) {
-    node = document.getElementById(placeholderId);
-    node.innerHTML= '';
-    node.removeAttribute('style');
-}
+    emptyFieldMessage : 'no se puede hacer el cálculo porque faltan datos, asegúrese de llenar todos los campos con letras o números según sea el caso.',
 
-function clearFields(arrayOfFieldIds) {
-    for (elementId of arrayOfFieldIds) {
-        document.getElementById(elementId).value = '';
-    }
-}
-
-function isEmpty(arrayOfFieldIds) {
-    for (elementId of arrayOfFieldIds){
-        if (document.getElementById(elementId).value == ''){
-                return true;
-                break;
+    clearPlaceholder : function (placeholderId) {
+        node = document.getElementById(placeholderId);
+        node.innerHTML= '';
+        node.removeAttribute('style');
+    },
+    
+    clearFields : function (arrayOfFieldIds) {
+        for (elementId of arrayOfFieldIds) {
+            document.getElementById(elementId).value = '';
         }
-    }
-}
+    },
 
-function displayError(placeholderId, errorMessage) {
-    if (screen.width < 768) {
-        alert('Error: ' + errorMessage);
-    } else {
-        
-        errorNode = document.getElementById(placeholderId);
-        warningNode = document.createElement('div');
-        warningNode.setAttribute('class','alert alert-danger');
-        warningNode.setAttribute('role','alert');
-        
-        warningMessageTitle = document.createElement('strong');
-        warningMessageTitle.appendChild(document.createTextNode('ERROR: '));
-        
-        warningMessageContent = document.createTextNode(errorMessage);
-    
-        warningNode.appendChild(warningMessageTitle);
-        warningNode.appendChild(warningMessageContent);
-        
-        errorNode.appendChild(warningNode);
-    }
-}
+    isEmpty : function (arrayOfFieldIds) {
+        for (elementId of arrayOfFieldIds){
+            if (document.getElementById(elementId).value == ''){
+                    return true;
+            }
+        }
+    },
 
-function displayMessage(placeholderId, messageText) {
+    displayError : function (placeholderId, errorMessage) {
+        if (screen.width < 768) {
+            alert('Error: ' + errorMessage);
+        } else {
+            errorNode = document.getElementById(placeholderId);
+            warningNode = document.createElement('div');
+            warningNode.setAttribute('class','alert alert-danger');
+            warningNode.setAttribute('role','alert');
+                
+            warningMessageTitle = document.createElement('strong');
+            warningMessageTitle.appendChild(document.createTextNode('ERROR: '));
     
-    if (screen.width < 768) {
-        alert(messageText);
-    } else {
-        messageNode = document.getElementById(placeholderId);
-        resultNode = document.createElement('div');
-        resultNode.setAttribute('class','alert alert-success');
-        resultNode.setAttribute('style', 'text-align: center;');
-        resultNode.setAttribute('role','alert');
-        
-        resultMessageContent = document.createTextNode(messageText);
-    
-        resultNode.appendChild(resultMessageContent);
-        
-        messageNode.appendChild(resultNode);
-    }
-}
+            warningMessageContent = document.createTextNode(errorMessage);
 
-function calculatePrice(wordcountContainerId, rateContainerId, messagePlaceholderId) {
-        clearPlaceholder(messagePlaceholderId);
+            warningNode.appendChild(warningMessageTitle);
+            warningNode.appendChild(warningMessageContent);
+
+            errorNode.appendChild(warningNode);
+        }
+    },
+    
+    displayMessage : function (placeholderId, messageText) {   
+
+        if (screen.width < 768) {
+            alert(messageText);
+        } else {
+            messageNode = document.getElementById(placeholderId);
+            resultNode = document.createElement('div');
+            resultNode.setAttribute('class','alert alert-success');
+            resultNode.setAttribute('style', 'text-align: center;');
+            resultNode.setAttribute('role','alert');
+            
+            resultMessageContent = document.createTextNode(messageText);
         
-        if (isEmpty([wordcountContainerId, rateContainerId])) {
-            displayError(messagePlaceholderId, emptyFieldMessage);
+            resultNode.appendChild(resultMessageContent);
+            
+            messageNode.appendChild(resultNode);
+        }
+    },
+
+    calculatePrice : function (wordcountContainerId, rateContainerId, messagePlaceholderId) {
+        this.clearPlaceholder(messagePlaceholderId);
+        
+        if (this.isEmpty([wordcountContainerId, rateContainerId])) {
+            this.displayError(messagePlaceholderId, this.emptyFieldMessage);
         } else {
             words = document.getElementById(wordcountContainerId).value;
             rate = document.getElementById(rateContainerId).value;
             result = 'La traducción valdría ' + (words * rate).toLocaleString() + ' pesos';
-            displayMessage(messagePlaceholderId,result);
+            this.displayMessage(messagePlaceholderId,result);
         }
-}
+    },
 
-function countWords(textContainerId, messagePlaceholderId) {
-        clearPlaceholder(messagePlaceholderId);
-        
-        if (isEmpty([textContainerId])) {
-            displayError(messagePlaceholderId, emptyFieldMessage);
+    countWords : function (textContainerId, messagePlaceholderId) {
+        this.clearPlaceholder(messagePlaceholderId);
+
+        if (this.isEmpty([textContainerId])) {
+            this.displayError(messagePlaceholderId, this.emptyFieldMessage);
         } else {
             text = document.getElementById(textContainerId).value;
             result = 'El segmento analizado tiene ' + text.split(' ').length + ' palabras.';
-            displayMessage(messagePlaceholderId, result);
+            this.displayMessage(messagePlaceholderId, result);
         }
-}
+    },
 
-function findRemainingWords(quotaValueId, initialWordsId, currentWordsId, messagePlaceholderId) {
-    clearPlaceholder(messagePlaceholderId);
-    
-    if (isEmpty([quotaValueId, initialWordsId, currentWordsId])) {
-        displayError(messagePlaceholderId, emptyFieldMessage);
-    } else {
-        dailyQuota = document.getElementById(quotaValueId).value;
-        initiallyRemainingWords = document.getElementById(initialWordsId).value;
-        currentlyRemainingWords = document.getElementById(currentWordsId).value;
+    findRemainingWords : function findRemainingWords(quotaValueId, initialWordsId, currentWordsId, messagePlaceholderId) {
+        this.clearPlaceholder(messagePlaceholderId);
         
-        translatedSoFar = initiallyRemainingWords - currentlyRemainingWords;
-        remainingWords = dailyQuota - translatedSoFar;
-        if (remainingWords < 0) {
-            result = 'Van ' + translatedSoFar + (' palabras traducidas. La cuota era ') + dailyQuota + '. eso significa que se han traducido ' + (translatedSoFar - dailyQuota) + ' palabras de más.';
+        if (this.isEmpty([quotaValueId, initialWordsId, currentWordsId])) {
+            this.displayError(messagePlaceholderId, this.emptyFieldMessage);
         } else {
-            result= 'Se han traducido ' + translatedSoFar + ' palabras y faltan ' + remainingWords + ' para terminar.';
-        }
-        displayMessage(messagePlaceholderId,result);
-    }
-}
+            dailyQuota = document.getElementById(quotaValueId).value;
+            initiallyRemainingWords = document.getElementById(initialWordsId).value;
+            currentlyRemainingWords = document.getElementById(currentWordsId).value;
 
-function estimateDays(volumeContainerId, speedContainerId, messagePlaceholderId) {
-        clearPlaceholder(messagePlaceholderId);
+            translatedSoFar = initiallyRemainingWords - currentlyRemainingWords;
+            remainingWords = dailyQuota - translatedSoFar;
+            if (remainingWords < 0) {
+                result = 'Van ' + translatedSoFar + (' palabras traducidas. La cuota era ') + dailyQuota + '. eso significa que se han traducido ' + (translatedSoFar - dailyQuota) + ' palabras de más.';
+            } else {
+                result= 'Se han traducido ' + translatedSoFar + ' palabras y faltan ' + remainingWords + ' para terminar.';
+            }
+            this.displayMessage(messagePlaceholderId,result);
+        }
+    },
+
+    estimateDays : function (volumeContainerId, speedContainerId, messagePlaceholderId) {
+        this.clearPlaceholder(messagePlaceholderId);
         
-        if (isEmpty([volumeContainerId, speedContainerId, messagePlaceholderId])) {
-                displayError(messagePlaceholderId, emptyFieldMessage);
+        if (this.isEmpty([volumeContainerId, speedContainerId, messagePlaceholderId])) {
+                this.displayError(messagePlaceholderId, this.emptyFieldMessage);
         } else {
                 volume = document.getElementById(volumeContainerId).value;
                 speed = document.getElementById(speedContainerId).value;
                 result = 'El trabajo se tardará aproximadamente unos ' + (volume/speed).toLocaleString() + ' días.';
-                displayMessage(messagePlaceholderId, result);
+                this.displayMessage(messagePlaceholderId, result);
         }
-}
+    }
+} 
